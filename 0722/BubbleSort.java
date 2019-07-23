e007e528d4be93d7f549f89f205d02885d07502e
smile486z <smile486z@naver.com>
2019-07-23 10:14:55 +0900

    20190722

---
+++ 0722/BubbleSort.java
@@ -0,0 +1,38 @@
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BubbleSort {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int N = sc.nextInt();
+		int[] arr = new int[N];
+
+		for (int i = 0; i < N; i++) {
+			arr[i] = sc.nextInt();
+		}
+		
+		for (int i = N - 1; i > 0; i--) {
+			boolean isSwap = false;
+			for (int j = 0; j < i; j++) { 
+				
+				if (arr[j] > arr[j + 1]) {
+					int temp = arr[j];
+					arr[j] = arr[j + 1];
+					arr[j + 1] = temp;
+					isSwap = true;
+				}
+
+			}
+			if (!isSwap)
+				break;
+		}
+		
+		System.out.println(Arrays.toString(arr));
+
+	}
+
+}
---
+++ 0722/CountingSort.java
@@ -0,0 +1,37 @@
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class CountingSort {	
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int[] arr = new int[] { 0, 4, 1, 3, 1, 2, 4, 1 };
+		
+		System.out.println(Arrays.toString(arr));
+		System.out.println(Arrays.toString(countingSort(arr)));
+
+	}
+
+	private static int[] countingSort(int[] arr) {
+		int[] counts = new int[5];
+		
+		for(int i =0;i<arr.length;i++) {
+			counts[arr[i]]++;
+		}
+		
+		for(int i=1;i<counts.length;i++) {
+			counts[i] = counts[i-1]+counts[i];
+		}
+		
+		int[] newArr = new int[arr.length];
+		for (int i = arr.length-1;i>=0;i--) {/
+			
+			newArr[--counts[arr[i]]] = arr[i];
+		}
+		return newArr;
+	}
+
+}
