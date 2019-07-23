876041e5e2e6b7dbba6a29be459c12eb1f1f605e
seoyury <smile486z@naver.com>
2019-07-24 08:57:16 +0900

    20190723

---
+++ 0722/HW1_BinarySearch.java
@@ -0,0 +1,46 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class HW1_BinarySearch_서유리 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int N = sc.nextInt();
+		int[] arr = new int[N];
+		for (int i = 0; i < N; i++) {
+			arr[i] = sc.nextInt();
+
+		}
+
+		Arrays.sort(arr);
+		System.out.println(Arrays.toString(arr));
+		int M = sc.nextInt();
+
+		int first = 0;
+		int last = arr.length - 1;
+		int mid = (first + last) / 2;
+
+		int result = 0;
+
+		int x = sc.nextInt();// ã�� ��
+
+		while (first <= last) {
+			if (arr[mid] == x) {
+				result = mid;
+				break;
+			} else if (arr[mid] < x) {
+				first = mid + 1;
+				mid = (last + first) / 2;
+
+			} else {// x < (arr[mid])
+				last = mid - 1;
+				mid = (last + first) / 2;
+
+			}
+
+		}
+		System.out.println(result);
+	}
+
+}
---
+++ 0722/Solution_D4_1210_Ladder1_2.java
@@ -0,0 +1,56 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Solution_D4_1210_Ladder1_2{
+
+	static int[][] arr;
+
+	static int result = 0;
+
+	public static void main(String[] args) throws FileNotFoundException {
+		System.setIn(new FileInputStream("input.txt"));
+		Scanner sc = new Scanner(System.in);
+
+		for (int t = 1; t <= 10; t++) {
+			int n = sc.nextInt();// 1
+			arr = new int[102][102];
+			int x = 0;
+			int y = 0;
+
+			for (int i = 1; i <= 100; i++) {
+				for (int j = 1; j <= 100; j++) {
+					arr[i][j] = sc.nextInt();
+					if (arr[i][j] == 2) {
+						x = i;
+						y = j;
+
+					}
+				}
+			}
+			// System.out.println(y);
+			for (int i = 99; i > 0; i--) {
+				x--;
+				if (arr[x][y + 1] == 1) {
+					while (true) {
+						y++;
+						if (arr[x][y + 1] != 1)
+							break;
+
+					}
+				} else if (arr[x][y - 1] == 1) {
+					while (true) {
+						y--;
+						if (arr[x][y - 1] != 1)
+							break;
+
+					}
+				}
+			}
+
+			System.out.println("#" + n + " " + y);
+		}
+
+	}
+
+}
