936c7574f0d818f83f06b6685fbcebdea6f5a385
smile486z <smile486z@naver.com>
2019-07-24 17:49:17 +0900

    20190724

--- 20190724/ProductTest.java
+++ 20190724/ProductTest.java
@@ -4,9 +4,9 @@ public class ProductTest {
 	//TV의정보(int제품번호, String제품이름, int가격, int수량,int인치, String디스플레이타입)
 	//냉장고정보(int제품번호, String제품이름, int가격, int수량,int용량)
 	public static void main(String[] args) {
-		TV tv1 = new TV("1234","tvtv!@!",200000,4,40,"$@@@@");
+		TV tv1 = new TV("1234","tvtv",200000,4,40,"C");
 		System.out.println(tv1.toString());
-		TV tv2 = new TV("432","tvtv2",500000,-2,40,"%#$%$#%");
+		TV tv2 = new TV("432","tvtv2",500000,-2,40,"D");
 		System.out.println(tv2.toString());
 		TV tv3 = new TV("5332","333tvtv",150000,40,20,"$");
 		System.out.println(tv3.toString());
