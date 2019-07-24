a03fd6e84416f12e5a843a7f2e0ce9aced17de5f
seoyury <smile486z@naver.com>
2019-07-24 09:55:40 +0900

    20190723

---
+++ 20190723/Hanoi.java
@@ -0,0 +1,32 @@
+package com.ssafy.recursive;
+
+import java.util.Scanner;
+
+public class Hanoi {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		Hanoi(N, 1, 2, 3);
+		int count = (int) Math.pow(2, N) - 1;
+	}
+	//from은 현재 위치, to는 목적지, by는 옮기기 위해 이동할 곳
+	private static void Hanoi(int n, int from, int by, int to) {
+		/*if (n == 0) {//123
+			return;
+		}*/
+		if(n==1) {//한개 남았을때는 세번째자리로
+			System.out.println(from + " "+to);
+			return;
+		}
+		//n-1 개의 원판을 2번째 기둥으로 옮기기
+		Hanoi(n - 1, from, to, by);
+		
+		//제일 큰 원판을 세번째 자리로 옮기기
+		System.out.println(from + " " + to);
+		
+		//2번째 기둥에 있는 원판을 세번째 자리로 옮기기
+		Hanoi(n - 1, by, from, to);
+	}
+
+}
---
+++ 20190723/Hanoi2.java
@@ -0,0 +1,35 @@
+package com.ssafy.recursive;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Hanoi2 {
+	public static BufferedReader br;
+	public static BufferedWriter bw;
+
+	public static void main(String[] args) throws IOException {
+		br = new BufferedReader(new InputStreamReader(System.in));
+		bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int N = Integer.parseInt(br.readLine());
+		int count = (int) Math.pow(2, N) - 1;
+
+		System.out.println(count);
+		hanoi(N, 1, 2, 3);
+
+		bw.flush();
+	}
+
+	public static void hanoi(int n, int from, int by, int to) throws IOException {
+		if (n == 0) {
+			return;
+		}
+
+		hanoi(n - 1, from, to, by);
+		bw.write(from + " " + to + "\n");
+		bw.flush();
+		hanoi(n - 1, by, from, to);
+	}
+}
